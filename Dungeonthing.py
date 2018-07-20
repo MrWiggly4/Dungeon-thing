a7c3413623afa5bcbe1e2d197be65d8957b205ce
MrWiggly4 <MrWiggly4>
2018-07-20 14:45:50 -0500

    Initial commit

---
+++ Dungeonthing.py
@@ -0,0 +1,14 @@
+import time
+print("INSERT TITLE HERE ILL JUST THINK OF IT LATER")
+x=input("Please enter your name")
+print(x+ ",you are an amateur mage in the land of Tork. Your master has assigned you with collecting a key in the dungeons of sslurp. Your goal is to make it back alive and back to your master to return the key") 
+time.sleep(5)
+print("Now that you are at the entrance of the dungeon you go inside.")
+print("There is a long narrow path in front of you and you walk down the hall to find a small necklace with a blue gem, you recognize this necklace as a charm of shocking.")
+time.sleep(2)
+print("You pick up the necklace and put it around your neck. You can now produce electric shocks from your hands.")
+n=input("You turn to your left and see an alarmingly large spider that looks rather hungry. Will you shoot an electric blast or run the other way?")
+if n==('run the other way'):
+    print("As you flee from possible death down another corridor, you see a fork in the road. Will you go left or right?")
+if n=='shoot an electric blast':
+    print("
